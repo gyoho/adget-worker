39cceb2e0f88bc68c2705f0f4cf058e75db43868
Yaopeng Wu <yaopeng.wu@chartboost.com>
2017-08-10 11:44:29 -0700

    Handle failure and log at higher level

--- src/main/scala/com/gyoho/adget/Advertiser.scala
+++ src/main/scala/com/gyoho/adget/Advertiser.scala
@@ -18,34 +18,38 @@ trait Advertiser extends LazyLogging {
   implicit def ec: ExecutionContext
   val TIMEOUT_IN_MILLIS = 5000
 
-  def sendRequest(timeoutInMillis: Long): String = {
-    logger.info(s"timeout = $timeoutInMillis milliseconds")
-    val currentTime = System.currentTimeMillis()
-
-    val responseFuture = Future {
-      Retry(ScalaHttpClient.httpGet(new URL(url))) match {
-        case RetrySuccess(res) => res
-        case RetryFailure(ex) =>
-          logger.info(
-            s"Failed to get ads from advertiser=$name. Max retry exceeds. ex=${ex.getMessage}")
-          ""
-      }
+  def sendRequest(timeoutInMillis: Long): Try[String] = {
+    val responseFuture: Future[Retry[String]] = Future {
+      Retry(ScalaHttpClient.httpGet(new URL(url)))
     }
 
     Try(Await.result(responseFuture, timeoutInMillis.millisecond)) match {
-      case Success(res) => res
-      case Failure(ex) =>
-        logger.info(s"Failed to get ads from advertiser=$name. Request timeout. ex=${ex.getMessage}")
-        logger.info(s"took ${System.currentTimeMillis() - currentTime} milliseconds")
-        ""
+      case Success(res) => res match {
+        case RetrySuccess(data) => Success(data)
+        case RetryFailure(ex) => Failure(ex)
+      }
+      case Failure(ex) => Failure(ex)
     }
   }
 
   protected def parse(body: String): Seq[Ad]
 
   def getAds(timeoutInMillis: Long = TIMEOUT_IN_MILLIS): Seq[Ad] = {
+    logger.info(s"timeout = $timeoutInMillis milliseconds")
+    val currentTime = System.currentTimeMillis()
+
     val response = sendRequest(timeoutInMillis)
-    parse(response)
+
+    logger.info(s"took ${System.currentTimeMillis() - currentTime} milliseconds")
+
+    val resToparse = response match {
+      case Success(res) => res
+      case Failure(ex) =>
+        logger.info(s"Failed to get ads from advertiser=$name. Request timeout. ex=${ex.getMessage}")
+        ""
+    }
+
+    parse(resToparse)
   }
 }
 
